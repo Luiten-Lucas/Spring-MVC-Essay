9200545850c54ade769c5b8ee7a898bf7abd6881
Luiten-Lucas <lluiten83@gmail.com>
2023-07-15 10:02:49 -0300

    Project start

---
+++ CoreWebApi/src/main/java/io/zulu/webapi/CoreWebApiApplication.java
@@ -0,0 +1,13 @@
+package io.zulu.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreWebApiApplication.class, args);
+	}
+
+}
---
+++ CoreWebApi/src/test/java/io/zulu/webapi/CoreWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zulu.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
